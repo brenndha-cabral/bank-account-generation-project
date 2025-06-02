f08247d188d7fc8a1f6bb2804e92ddcd2e65701b
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-02 15:11:49 -0300

    feat: create about function);

---
+++ about.ts
@@ -0,0 +1,9 @@
+export function about(): void {
+    console.log(`
+*****************************************************
+Projeto Desenvolvido por: 
+Brenndha Cabral
+https://github.com/brenndha-cabral
+*****************************************************
+`);
+}
