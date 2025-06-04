9ca555151491192cd2bd19f9480b2f925813f131
brenndha-cabral <cabralbrenndha@gmail.com>
2025-06-04 17:27:33 -0300

    feat: create accountRepository

---
+++ src/repository/AccountRepository.ts
@@ -0,0 +1,13 @@
+import { Account } from "../models/Account";
+
+export interface AccountRepository {
+    findAccountByNumber(number: number): void;
+    listAllAccounts(): void;
+    registerAccount(account: Account): void;
+    updateAccount(account: Account): void;
+    deleteAccount(number: number): void;
+
+    withdraw(number: number, value: number): void;
+    deposit(number: number, value: number): void;
+    transfer(originNumber: number, destinationNumber: number, value: number): void;
+};
